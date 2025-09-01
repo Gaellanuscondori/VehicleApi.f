b9f73c23ae4e425084ecf74c86ac04de11ebcfd2
Gael Condori <a2024113770@estudiantes.upsa.edu.bo>
2025-09-01 19:53:38 -0400

    segundo commit

--- Program.cs
+++ Program.cs
@@ -1,23 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Services
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer(); // Necesario para exponer el esquema OpenAPI
+builder.Services.AddSwaggerGen();           // Swashbuckle (Swagger UI)
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();   // /swagger/v1/swagger.json
+    app.UseSwaggerUI(); // /swagger
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
